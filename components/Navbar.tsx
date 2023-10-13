c0ce19dc43bb40227a6632549db3089ef7e62e63
valensixx <valensixx@gmail.com>
2023-10-13 15:04:09 +0300

    add data in index.ts

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,13 @@ import Image from "next/image"
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
         <Link href="/">
-            <Image src = "/hilink-logo.svg"  alt="logo" width={74} height={29} />
+            <Image src = "hilink-logo.svg"  alt="logo" width={74} height={29} />
+
+            <ul className="hidden f-full gap-12 lg:flex">
+                
+            </ul>
         </Link>
     </nav>
   )
