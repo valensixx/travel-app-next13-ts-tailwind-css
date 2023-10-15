99e7cbccb0d49eace402923517c5c49cc8f07936
valensixx <valensixx@gmail.com>
2023-10-15 17:01:05 +0300

    working on GetApp.tsx

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import Button from "./Button"
+
 
 const GetApp = () => {
   return (
-    <div>GetApp</div>
+    <section className='flexCenter w-full flex-col pb-[100px]'>
+      <div className='get-app'>
+        <div className='z-20 flex w-full flex-1 flex-col items-start justify-center gap-12'>
+          <h2 className='bold-40 lg:bold-64 xl:max-w-[320px]'>
+            Get for free now
+          </h2>
+          <p className='regular-16 text-gray-10'>
+            Available on iOS and Android
+          </p>
+          <div>
+            <Button
+              type='button'
+              title='Download App'
+              icon='/apple.svg'
+              variant='btn_white'
+              full
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
