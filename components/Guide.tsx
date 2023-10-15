718e4682b22ba3c876ff8aa397375d7e4201f378
valensixx <valensixx@gmail.com>
2023-10-15 15:33:02 +0300

    working on Guide.tsx

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import Image from "next/image"
 
 const Guide = () => {
   return (
-    <div>Guide</div>
+    <section className='flexCenter flex-col'>
+      <div className='border-2 border-red-500 padding-container max-container w-full pb-24'>
+        <Image 
+          src='/camp.svg'
+          alt='camp'
+          width={50}
+          height={50}
+        />
+        <p className='uppercase regular-18 -mt-1 mb-3 text-green-50'>
+          We are here for you!
+        </p>
+        <div className='flex flex-wrap justify-between gap-5 lg:gap-10'>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
